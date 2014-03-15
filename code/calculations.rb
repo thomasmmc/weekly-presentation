12cb421ba3e49daad464e540d3a4cf69107ddf4b
thomasmmc <thomasmmc@gmail.com>
2014-03-15 11:14:44 -0400

    initial commit

---
+++ code/calculations.rb
@@ -0,0 +1,19 @@
+class CALC
+	def eol_days(date,os)
+	 	doy = date.strftime("%j")
+	 	if os == 'xp'
+	 		cutoverdate = Time.new(2014,4,8,0,0,0,"-05:00")
+	 	elsif os == '2003'
+	 		cutoverdate = Time.new(2015,7,14,0,0,0,"-05:00")
+	 	end
+	 	doco = cutoverdate.strftime("%j")
+	 	yeardiff = cutoverdate.year - date.year
+	 	daysleft = doco.to_i - doy.to_i
+	 	# case yeardiff
+	 	# when 0
+	 	# 	daysleft
+	 	# else
+	 	daysleft + (365*yeardiff)
+	 	# end
+ 	end
+end
